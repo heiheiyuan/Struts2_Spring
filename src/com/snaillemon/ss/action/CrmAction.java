318cf916ad89884859aad362127ccd7ea491f9f1
SnailLemon <hyy8516998@163.com>
2016-11-09 08:45:52 +0800

    I changed it to the form of annotation

--- src/com/snaillemon/ss/action/CrmAction.java
+++ src/com/snaillemon/ss/action/CrmAction.java
@@ -1,29 +1,32 @@
 package com.snaillemon.ss.action;
 
-import com.opensymphony.xwork2.Action;
-import com.opensymphony.xwork2.ActionSupport;
-import com.snaillemon.ss.service.CrmService;
 import com.snaillemon.ss.service.impl.CrmServiceImpl;
+import org.springframework.stereotype.Controller;
+
+import javax.annotation.Resource;
 
 /**
  * Created by GoodBoy on 11/8/2016.
  */
-public class CrmAction implements Action {
+@Controller("crmAction")
+public class CrmAction {
 
+    @Resource(name="crmServiceImpl")
     public CrmServiceImpl crmService;
 
+    public String execute() throws Exception {
+        System.out.println("web层gogogo");
+        crmService.add();
+        return "success";
+    }
+
+/*
     public CrmService getCrmService() {
         return crmService;
     }
 
     public void setCrmService(CrmServiceImpl crmService) {
         this.crmService = crmService;
-    }
+    }*/
 
-    @Override
-    public String execute() throws Exception {
-        System.out.println("web层gogogo");
-        crmService.add();
-        return "success";
-    }
 }
--- src/com/snaillemon/ss/service/impl/CrmServiceImpl.java
+++ src/com/snaillemon/ss/service/impl/CrmServiceImpl.java
@@ -2,23 +2,30 @@ package com.snaillemon.ss.service.impl;
 
 import com.snaillemon.ss.dao.CrmDao;
 import com.snaillemon.ss.service.CrmService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * Created by GoodBoy on 11/8/2016.
  */
+@Service("crmServiceImpl")
 public class CrmServiceImpl implements CrmService{
+
+    @Resource(name="crmDaoImpl")
     public CrmDao crmDao;
+
     @Override
     public void add() {
         System.out.println("service实现类gogogo");
         crmDao.add();
     }
 
-    public CrmDao getCrmDao() {
+    /*public CrmDao getCrmDao() {
         return crmDao;
     }
 
     public void setCrmDao(CrmDao crmDao) {
         this.crmDao = crmDao;
-    }
+    }*/
 }
